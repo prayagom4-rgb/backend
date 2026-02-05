18a030c776f3b7e1aaba2f5f8daad8ac7d2a12dc
Alok Mani Tripathi <eyalokmani@gmail.com>
2026-02-05 07:59:08 +0400

    fixes

--- services/oauthClient.js
+++ services/oauthClient.js
@@ -5,14 +5,17 @@ const CLIENT_ID =
   "497105170769-jovr105n48s95l213oq6n470el356ml1.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN =
-  "1//04eIzv6PqTShGCgYIARAAGAQSNwF-L9Ir0eaxPm7AJaksRGRlnPNdZF7QdJZveHyicF0kEaqhVmQONsbvMOjWychuhXIf2qaPZTw";
 
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+// 👇 Instead of storing in .env, you can pass refreshToken when you import
+function createOAuthClient(refreshToken) {
+  const oAuth2Client = new google.auth.OAuth2(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    REDIRECT_URI
+  );
 
-export default oAuth2Client;
+  oAuth2Client.setCredentials({ refresh_token: refreshToken });
+  return oAuth2Client;
+}
+
+export default createOAuthClient;
